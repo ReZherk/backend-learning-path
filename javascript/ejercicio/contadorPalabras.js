c2902590111da5f36f564bc304e79a3546469a7b
ReZherk <patrickcomeresbueno@gmail.com>
2026-02-23 13:13:20 -0500

    xd

--- javascript/ejercicio/contadorPalabras.js
+++ javascript/ejercicio/contadorPalabras.js
@@ -2,4 +2,9 @@ const fs = require("fs");
 
 //console.log(typeof fs);
 
-const content = fs.readFileSync();
+const content = fs.readFileSync(
+  "../teoria/paradigma-funciones-vs-clases.md",
+  "utf8",
+);
+
+const wordCount = content.split;
